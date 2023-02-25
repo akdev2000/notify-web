bca7a57714432d1b87ce478beb01b39cb5da6485
Aanandakrishnan <aanandakrishnan.d@gogosoon.com>
2023-02-25 11:19:34 +0530

    issue in generating qr code

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -1,5 +1,5 @@
 export const generateRandomString = (length: number) => {
-    const charactes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*/";
+    const charactes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#%^&*/";
     let res = "";
     for(let i=0;i<length;i++) {
         res = res + charactes.charAt(Math.floor(Math.random()*(charactes.length)));
